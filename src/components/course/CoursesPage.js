fdb1f6b147fb5b4ce96a36a9ced61f136ad7a10f
Brandon DonatoLong <long.brandonoit@gmail.com>
2017-07-20 21:49:31 -0700

    changes in 9.3. Binding in the constructor and ES6

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -9,14 +9,36 @@ class CoursesPage extends React.Component {
         super(props, context);
 
         this.state = {
-            course:{title:null}
+            course:{title:""}
         };
+
+        this.onTitleChange = this.onTitleChange.bind(this);
+        this.onClickSave = this.onClickSave.bind(this);
+    }
+
+    onTitleChange(event){
+        const course = this.state.course;
+        course.title = event.target.value;
+        this.setState({course:course});
+    }
+
+    onClickSave(){
+        alert(`saving ${this.state.course.title}`);
     }
 
     render() {
         return (
             <div>
                 <h1>Courses</h1>
+                <input
+                    type="text"
+                    onChange={this.onTitleChange}
+                    value={this.state.course.title} />
+
+                <input
+                    type="submit"
+                    value="Save"
+                    onClick={this.onClickSave} />
             </div>
         );
     }
