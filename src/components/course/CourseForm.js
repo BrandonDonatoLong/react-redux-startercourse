14baffe35e2942a75164e9ffec4fa87e2591e2b8
Brandon Donato-Long <brandon.long@respingaming.net>
2017-07-21 15:35:25 -0700

    Added course form stateless component. Will need to set this up at home as well

---
+++ src/components/course/CourseForm.js
@@ -0,0 +1,14 @@
+import React, {PropTypes} from 'react';
+
+const CourseForm = (props) => {
+    return (
+
+    );
+}
+
+CourseForm.propTypes = {
+    myProp: PropTypes.string.isRequired
+};
+
+export default CourseForm
+    
